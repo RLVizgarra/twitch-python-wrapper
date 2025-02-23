79a49c4f218e24a3c949f0955fa63a680130746d
Rodrigo Lion Vizgarra <rodrigolionvizgarra@gmail.com>
2025-02-23 06:32:58 -0300

    Removed plain-text token using .env Rookie mistake, forgot about .env files and just used a token in the code

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -1,8 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
 from api.client import APIClient
 from api.enums import UserType, BroadcasterType
 from api.objects import User
 
-client = APIClient("***REMOVED***", "***REMOVED***")
+load_dotenv()
+
+client = APIClient(os.getenv("CLIENT_ID"), os.getenv("ACCESS_TOKEN"))
 
 def test_get_user():
     twitchdev = User(id='141981764', login='twitchdev', display_name='TwitchDev', type=UserType.NORMAL, broadcaster_type=BroadcasterType.PARTNER, description='Supporting third-party developers building Twitch integrations from chatbots to game integrations.', profile_image_url='https://static-cdn.jtvnw.net/jtv_user_pictures/8a6381c7-d0c0-4576-b179-38bd5ce1d6af-profile_image-300x300.png', offline_image_url='https://static-cdn.jtvnw.net/jtv_user_pictures/3f13ab61-ec78-4fe6-8481-8682cb3b0ac2-channel_offline_image-1920x1080.png', email=None, created_at=1481747548)
