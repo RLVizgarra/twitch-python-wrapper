5a3e7b952b30d5380d709930ece7113093e5364f
Rodrigo Lion Vizgarra <rodrigolionvizgarra@gmail.com>
2025-02-25 23:49:32 -0300

    Edited tests accordingly to previous commit

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -13,27 +13,27 @@ class TestAPIClient:
         first_user = 95983915723945059
         second_user = -4084956926418039017
 
-        assert self.client.get_users(login="foo_bar_baz_qux") is None
-        assert hash(self.client.get_users(login="twitchdev")) == first_user
-        assert hash(self.client.get_users(user_id="141981764")) == first_user
-        us = self.client.get_users(login=["twitchdev", "twitch"])
+        assert self.client.users.get_users(login="foo_bar_baz_qux") is None
+        assert hash(self.client.users.get_users(login="twitchdev")) == first_user
+        assert hash(self.client.users.get_users(user_id="141981764")) == first_user
+        us = self.client.users.get_users(login=["twitchdev", "twitch"])
         for u in us:
             assert hash(u) == first_user or hash(u) == second_user
-        us = self.client.get_users(user_id=["141981764", "12826"])
+        us = self.client.users.get_users(user_id=["141981764", "12826"])
         for u in us:
             assert hash(u) == first_user or hash(u) == second_user
-        us = self.client.get_users(login="twitchdev", user_id="12826")
+        us = self.client.users.get_users(login="twitchdev", user_id="12826")
         for u in us:
             assert hash(u) == first_user or hash(u) == second_user
 
     def test_get_videos(self):
-        assert self.client.get_videos(video_id="123456789123") is None
-        assert hash(self.client.get_videos(video_id="335921245")) == 4462134045647488878
+        assert self.client.videos.get_videos(video_id="123456789123") is None
+        assert hash(self.client.videos.get_videos(video_id="335921245")) == 4462134045647488878
 
     def test_get_clips(self):
-        assert self.client.get_clips(clip_id="AwkwardHelplessSalamanderSwiftRage") is None
-        assert hash(self.client.get_clips(clip_id="ObedientRelievedPepperoniSwiftRage")) == 4781649331344373151
-        assert hash(self.client.get_clips(broadcaster_id="141981764", first=5)) == 5772613717331245050
+        assert self.client.clips.get_clips(clip_id="AwkwardHelplessSalamanderSwiftRage") is None
+        assert hash(self.client.clips.get_clips(clip_id="ObedientRelievedPepperoniSwiftRage")) == 4781649331344373151
+        assert hash(self.client.clips.get_clips(broadcaster_id="141981764", first=5)) == 5772613717331245050
 
     def test_get_cheermotes(self):
-        assert hash(self.client.get_cheermotes(broadcaster_id="141981764")) == -3447302537155689151
+        assert hash(self.client.bits.get_cheermotes(broadcaster_id="141981764")) == -3447302537155689151
