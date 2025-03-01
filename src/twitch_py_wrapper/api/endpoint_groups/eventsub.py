db50ef8c8dbaa51bc4e512eeb28bf66fa7c0fe04
Rodrigo Lion Vizgarra <rodrigolionvizgarra@gmail.com>
2025-03-01 19:32:40 -0300

    Added get eventsub subscription endpoint

--- src/twitch_py_wrapper/api/endpoint_groups/eventsub.py
+++ src/twitch_py_wrapper/api/endpoint_groups/eventsub.py
@@ -2,7 +2,8 @@ import httpx
 from dateutil.parser import isoparse
 
 from twitch_py_wrapper.api.client import APIClient
-from twitch_py_wrapper.api.objects import Subscription
+from twitch_py_wrapper.api.enums import SubscriptionStatus
+from twitch_py_wrapper.api.objects import Subscription, Pagination
 from twitch_py_wrapper.shared_enums import SubscriptionType
 from twitch_py_wrapper.shared_objects import NotificationTransport
 
@@ -84,5 +85,57 @@ class EventSub:
         req.raise_for_status()
 
     # https://dev.twitch.tv/docs/api/reference/#get-eventsub-subscriptions
-    def get_eventsub_subscriptions(self):
-        pass
+    def get_eventsub_subscriptions(self,
+                                   status: SubscriptionStatus | None,
+                                   subscription_type: SubscriptionType | None,
+                                   user_id: str | None,
+                                   after: Pagination | None) -> None | tuple[tuple[Subscription, ...], int, int, int] | tuple[tuple[Subscription, ...], int, int, int, Pagination]:
+        url = self.client.users + "eventsub/subscriptions"
+
+        parameters = {}
+
+        optional_params = {
+            "status": status.value if status else None,
+            "type": subscription_type.value if subscription_type else None,
+            "user_id": user_id,
+            "after": after.cursor if after else None
+        }
+
+        if ("after" in optional_params and len(optional_params) > 2) or len(optional_params) > 1:
+            raise ValueError("Can't specify more than one filter")
+
+        for key, value in optional_params.items():
+            if value: parameters[key] = value
+
+        req = httpx.get(url,
+                        params=parameters,
+                        headers=self.client._headers,
+                        timeout=self.client._timeout)
+
+        req.raise_for_status()
+        res = req.json()
+
+        if len(res["data"]) < 1: return None
+
+        subscriptions = list()
+        for sub in res["data"]:
+            transport = NotificationTransport(method=sub["transport"]["method"],
+                                              callback=sub["transport"]["callback"] if "callback" in sub["transport"] else None,
+                                              secret=sub["transport"]["secret"] if "secret" in sub["transport"] else None,
+                                              session_id=sub["transport"]["session_id"] if "session_id" in sub["transport"] else None,
+                                              conduit_id=sub["transport"]["conduit_id"] if "conduit_id" in sub["transport"] else None,
+                                              connected_at=sub["transport"]["connected_at"] if "connected_at" in sub["transport"] else None,
+                                              disconnected_at=sub["transport"]["disconnected_at"] if "disconnected_at" in sub["transport"] else None)
+
+            subscriptions.append(Subscription(id=sub["id"],
+                                              status=sub["status"],
+                                              type=SubscriptionType(sub["type"]),
+                                              version=sub["version"],
+                                              condition=tuple(sorted((str(k), str(v)) for k, v in sub["condition"].items())),
+                                              created_at=int(isoparse(sub["created_at"]).timestamp()),
+                                              transport=transport,
+                                              cost=sub["cost"]))
+
+        if len(res["pagination"]) > 0: tuple(subscriptions), res["total"], res["total_cost"], res["max_total_cost"], Pagination(res["pagination"]["cursor"])
+
+        return tuple(subscriptions), res["total"], res["total_cost"], res["max_total_cost"]
