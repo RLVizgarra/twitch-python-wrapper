3d7372f2770958442f479b195fc17d951291b0a0
Rodrigo Lion Vizgarra <rodrigolionvizgarra@gmail.com>
2025-03-02 02:07:56 -0300

    Added get games endpoint Also added related tests

--- src/twitch_py_wrapper/api/endpoint_groups/games.py
+++ src/twitch_py_wrapper/api/endpoint_groups/games.py
@@ -48,5 +48,59 @@ class Games:
         return tuple(games)
 
     # https://dev.twitch.tv/docs/api/reference/#get-games
-    def get_games(self):
-        pass
+    def get_games(self,
+                  game_id: str | list[str] = None,
+                  name: str | list[str] = None,
+                  igdb_id: str | list[str] = None) -> Game | tuple[Game, ...] | None:
+        url = self.client._url + "games"
+
+        sum_of_lookups = 0
+
+        if isinstance(game_id, list):
+            sum_of_lookups += len(game_id)
+        elif game_id:
+            sum_of_lookups += 1
+
+        if isinstance(name, list):
+            sum_of_lookups += len(name)
+        elif name:
+            sum_of_lookups += 1
+
+        if isinstance(igdb_id, list):
+            sum_of_lookups += len(igdb_id)
+        elif igdb_id:
+            sum_of_lookups += 1
+
+        if sum_of_lookups > 100:
+            raise ValueError("Cannot look up for 100+ IDs and/or names")
+
+        parameters = {}
+
+        optional_params = {
+            "id": game_id,
+            "name": name,
+            "igdb_id": igdb_id
+        }
+
+        for key, value in optional_params.items():
+            if key: parameters[key] = value
+
+        req = httpx.get(url,
+                        params=parameters,
+                        headers=self.client._headers,
+                        timeout=self.client._timeout)
+        req.raise_for_status()
+        res = req.json()["data"]
+
+        if len(res) < 1: return None
+
+        games = list()
+        for game in res:
+            games.append(Game(id=game["id"],
+                              name=game["name"],
+                              box_art_url=game["box_art_url"],
+                              igdb_id=game["igdb_id"]))
+
+        if len(games) < 2: return games[0]
+
+        return tuple(games)
--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -10,21 +10,21 @@ class TestAPIClient:
     client = APIClient(os.getenv("CLIENT_ID"), os.getenv("APP_ACCESS_TOKEN"))
 
     def test_get_users(self):
-        first_user = 95983915723945059
-        second_user = -4084956926418039017
-
         assert self.client.users.get_users(login="foo_bar_baz_qux") is None
-        assert hash(self.client.users.get_users(login="twitchdev")) == first_user
-        assert hash(self.client.users.get_users(user_id="141981764")) == first_user
+        assert hash(self.client.users.get_users(login="twitchdev")) == 95983915723945059
+        assert hash(self.client.users.get_users(user_id="141981764")) == 95983915723945059
         us = self.client.users.get_users(login=["twitchdev", "twitch"])
         for u in us:
-            assert hash(u) == first_user or hash(u) == second_user
+            h = hash(u)
+            assert h == 95983915723945059 or h == -4084956926418039017
         us = self.client.users.get_users(user_id=["141981764", "12826"])
         for u in us:
-            assert hash(u) == first_user or hash(u) == second_user
+            h = hash(u)
+            assert h == 95983915723945059 or h == -4084956926418039017
         us = self.client.users.get_users(login="twitchdev", user_id="12826")
         for u in us:
-            assert hash(u) == first_user or hash(u) == second_user
+            h = hash(u)
+            assert h == 95983915723945059 or h == -4084956926418039017
 
     def test_get_videos(self):
         assert self.client.videos.get_videos(video_id="123456789123") is None
@@ -52,19 +52,35 @@ class TestAPIClient:
         assert hash(self.client.chat.get_emote_sets(emote_set_id="374814395")) == -1838195492418291180
 
     def test_get_channel_chat_badges(self):
-        assert hash(self.client.chat.get_channel_chat_badges("12826")) ==8269109171712613325
-        assert hash(self.client.chat.get_channel_chat_badges("197886470")) == -3076710540262200488
+        assert hash(self.client.chat.get_channel_chat_badges(broadcaster_id="12826")) ==8269109171712613325
+        assert hash(self.client.chat.get_channel_chat_badges(broadcaster_id="197886470")) == -3076710540262200488
 
     def test_get_global_chat_badges(self):
         assert hash(self.client.chat.get_global_chat_badges()) == -2677478051108591865
 
     def test_get_chat_settings(self):
-        assert hash(self.client.chat.get_chat_settings("141981764")) == 8585977543539151482
-        assert hash(self.client.chat.get_chat_settings("12826")) == -2700731223437012002
+        assert hash(self.client.chat.get_chat_settings(broadcaster_id="141981764")) == 8585977543539151482
+        assert hash(self.client.chat.get_chat_settings(broadcaster_id="12826")) == -2700731223437012002
 
     def test_get_user_chat_color(self):
-        assert hash(self.client.chat.get_user_chat_color("141981764")) == 8406074055112061289
-        assert hash(self.client.chat.get_user_chat_color("12826")) == 7968065401081294306
+        assert hash(self.client.chat.get_user_chat_color(user_id="141981764")) == 8406074055112061289
+        assert hash(self.client.chat.get_user_chat_color(user_id="12826")) == 7968065401081294306
 
     def test_get_content_classification_labels(self):
-        assert hash(self.client.ccls.get_content_classification_labels("en-US")) == -4466657788493661200
+        assert hash(self.client.ccls.get_content_classification_labels(locale="en-US")) == -4466657788493661200
+
+    def test_get_games(self):
+        assert hash(self.client.games.get_games(name="Just Chatting")) == -9147344911711028063
+        assert hash(self.client.games.get_games(game_id="509658")) == -9147344911711028063
+        gs = self.client.games.get_games(name=["Just Chatting", "Software and Game Development"])
+        for g in gs:
+            h = hash(g)
+            assert h == -9147344911711028063 or h == -4867558018606174595
+        gs = self.client.games.get_games(game_id=["509658", "1469308723"])
+        for g in gs:
+            h = hash(g)
+            assert h == -9147344911711028063 or h == -4867558018606174595
+        gs = self.client.games.get_games(name="Just Chatting", game_id="1469308723")
+        for g in gs:
+            h = hash(g)
+            assert h == -9147344911711028063 or h == -4867558018606174595
