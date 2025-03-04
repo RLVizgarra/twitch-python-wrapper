adcc5159b6581790b49b08cc417d4a30f9b8251e
Rodrigo Lion Vizgarra <rodrigolionvizgarra@gmail.com>
2025-03-04 03:42:24 -0300

    Improved EventSub client readability

--- src/twitch_py_wrapper/eventsub/client.py
+++ src/twitch_py_wrapper/eventsub/client.py
@@ -14,11 +14,13 @@ from twitch_py_wrapper.eventsub.objects import Metadata
 from twitch_py_wrapper.shared_enums import SubscriptionType, NotificationTransportMethod
 from twitch_py_wrapper.shared_objects import NotificationTransport
 
-BuiltinNotifications = Literal["builtins.message.welcome",
-                         "builtins.message.keepalive",
-                         "builtins.message.notification",
-                         "builtins.message.reconnect",
-                         "builtins.message.revocation"]
+BuiltinNotifications = Literal[
+    "builtins.message.welcome",
+    "builtins.message.keepalive",
+    "builtins.message.notification",
+    "builtins.message.reconnect",
+    "builtins.message.revocation"
+]
 
 class EventClient:
     session_id = None
@@ -26,7 +28,11 @@ class EventClient:
     _ssl.load_verify_locations(certifi.where())
 
 
-    def __init__(self, client_id: str, access_token: str, url: str = None, timeout: int = 10):
+    def __init__(self,
+                 client_id: str,
+                 access_token: str,
+                 url: str = None,
+                 timeout: int = 10) -> None:
         self._api = APIClient(client_id, access_token)
         self._timeout = timeout
 
@@ -35,10 +41,15 @@ class EventClient:
         self._previous_messages: list[str] = []
         self.__ws = None
 
-    def on(self, subscription: SubscriptionType | BuiltinNotifications, version: str = None, condition: dict = None):
+    def on(self,
+           subscription: SubscriptionType | BuiltinNotifications,
+           version: str = None,
+           condition: dict = None):
         validation = {
-            ((not version and not condition) and isinstance(subscription, SubscriptionType)): "When subscription is a SubscriptionType, version and condition must be supplied",
-            ((version and condition) and not isinstance(subscription, SubscriptionType)): "When subscription is a string, version and condition must not be supplied",
+            ((not version and not condition) and isinstance(subscription, SubscriptionType)):
+                "When subscription is a SubscriptionType, version and condition must be supplied",
+            ((version and condition) and not isinstance(subscription, SubscriptionType)):
+                "When subscription is a string, version and condition must not be supplied",
         }
 
         for validation_condition, error in validation.items():
@@ -48,9 +59,14 @@ class EventClient:
         def callback(function: Callable[[Metadata, dict], None]):
             self.register(function, subscription, version, condition)
             return function
+
         return callback
 
-    def register(self, function: Callable[[Metadata, dict], None], subscription: SubscriptionType | BuiltinNotifications, version: str = None, condition: dict = None):
+    def register(self,
+                 function: Callable[[Metadata, dict], None],
+                 subscription: SubscriptionType | BuiltinNotifications,
+                 version: str = None,
+                 condition: dict = None):
         handler = {
             "subscription": subscription,
             "condition": condition,
@@ -63,42 +79,54 @@ class EventClient:
 
         if self.session_id: self.__create_subscription(handler, self.session_id)
 
-    def __create_subscription(self, handler: dict, session_id: str):
+    def __create_subscription(self,
+                              handler: dict,
+                              session_id: str) -> None:
         if handler["registered"]: return
 
-        self._api.eventsub.create_eventsub_subscription(subscription_type=handler["subscription"],
-                                               version=handler["version"],
-                                               condition=handler["condition"],
-                                               transport=NotificationTransport(method=NotificationTransportMethod.WEBSOCKET,
-                                                                               callback=None,
-                                                                               secret=None,
-                                                                               session_id=session_id,
-                                                                               conduit_id=None,
-                                                                               connected_at=None,
-                                                                               disconnected_at=None))
+        self._api.eventsub.create_eventsub_subscription(
+            subscription_type=handler["subscription"],
+            version=handler["version"],
+            condition=handler["condition"],
+            transport=NotificationTransport(
+                method=NotificationTransportMethod.WEBSOCKET,
+                callback=None,
+                secret=None,
+                session_id=session_id,
+                conduit_id=None,
+                connected_at=None,
+                disconnected_at=None
+            )
+        )
         handler["registered"] = True
 
-    def _trigger_notification(self, metadata: Metadata, payload: dict):
+    def _trigger_notification(self,
+                              metadata: Metadata,
+                              payload: dict) -> None:
+        handler = None
+
         if not metadata.subscription_type or not isinstance(metadata.subscription_type, SubscriptionType): return
 
-        handler = None
         for event_handler in self._registered_event_handlers:
             if event_handler["subscription"] != metadata.subscription_type: continue
             handler = event_handler
             break
+
         if handler: handler["callback"](metadata, payload)
 
-    def _trigger_message(self, metadata: Metadata, payload: dict):
+    def _trigger_message(self,
+                         metadata: Metadata,
+                         payload: dict) -> None:
         message_type = metadata.message_type.value
-        if "_" in message_type:
-            message_type = message_type.split("_")[1]
-
+        if "_" in message_type: message_type = message_type.split("_")[1]
         subscription = "builtins.message." + message_type
         handler = None
+
         for event_handler in self._registered_event_handlers:
             if event_handler["subscription"] != subscription: continue
             handler = event_handler
             break
+
         if handler: handler["callback"](metadata, payload)
 
     async def connect(self):
@@ -120,22 +148,26 @@ class EventClient:
 
                         match metadata.message_type:
                             case MessageType.SESSION_WELCOME:
-                                session_id: str = payload["session"]["id"]
+                                self.session_id = payload["session"]["id"]
                                 if not self._timeout: self._timeout = payload["session"]["keepalive_timeout_seconds"]
-                                self.session_id = session_id
+
                                 for handler in self._registered_event_handlers:
                                     if not isinstance(handler["subscription"], SubscriptionType): continue
-                                    self.__create_subscription(handler, session_id)
+                                    self.__create_subscription(handler, self.session_id)
 
                             case MessageType.NOTIFICATION:
                                 self._trigger_notification(metadata, payload)
 
                             case MessageType.REVOCATION:
                                 handler = None
+
                                 for event_handler in self._registered_event_handlers:
-                                    if event_handler["subscription"] != SubscriptionType(payload["subscription"]["type"] or event_handler["condition"] != payload["subscription"]["condition"]): continue
+                                    if event_handler["subscription"] != SubscriptionType(payload["subscription"]["type"]
+                                    or event_handler["condition"] != payload["subscription"]["condition"]):
+                                        continue
                                     handler = event_handler
                                     break
+
                                 if handler: self._registered_event_handlers.remove(handler)
 
                             case MessageType.SESSION_RECONNECT:
@@ -145,11 +177,9 @@ class EventClient:
 
                         self._trigger_message(metadata, payload)
 
-                except asyncio.TimeoutError:
-                    await self.__ws.close()
+                except asyncio.TimeoutError: await self.__ws.close()
 
-                except websockets.ConnectionClosed:
-                    await self.__ws.close()
+                except websockets.ConnectionClosed: await self.__ws.close()
 
                 except asyncio.CancelledError or KeyboardInterrupt:
                     await self.__ws.close()
